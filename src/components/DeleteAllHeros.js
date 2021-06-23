1eb6eaaf215894ad816866578cfc49a2ac3d5c56
Tomjand <domagala.tomasz@gmail.com>
2021-06-24 01:29:21 +0200

    Delete src directory

--- src/components/DeleteAllHeros.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DeleteAllHeros = props => {
-  return <button onClick={props.onButtonClick}>delete All Heros</button>;
-};
-
-export default DeleteAllHeros;
--- src/reducers/moviesReducers.js
+++
@@ -1,6 +0,0 @@
-const movie = (stateMovie = "", action) => {
-  if (action.type === "UPDATE_MOVIE_TITLE") {
-    return action.text;
-  }
-  return stateMovie;
-};
