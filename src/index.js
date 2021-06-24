38d2ebe9c67c024cc03f3bd079ef8cfc4a88e351
Tomjand <domagala.tomasz@gmail.com>
2021-06-24 08:28:03 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import { createStore, combineReducers } from "redux";
 
 import "./styles.css";
 
-const initialState = "no movie";
+const initialState = {
+  text: "no movie",
+  heros: []
+};
 
-const ChangeText = (state = "", action) => {
+const ChangeState = (state = {}, action) => {
   if (action.type === "changeText") {
     return (state = "StarWars");
   }
   return state;
 };
 
-const mystore = createStore(ChangeText, initialState);
+const mystore = createStore(ChangeState, initialState);
 console.log("myAllData : ", mystore.getState());
 console.log("mystore : ", mystore);
 
@@ -26,13 +29,14 @@ mystore.dispatch({ type: "changeText" });
 function App() {
   return (
     <div className="App">
-      <h1>moj kurs Redux lekcja 03 ver. 2</h1>
-      <h3>zmiana stanu w postaci tekstowej</h3>
+      <h1>moj kurs Redux lekcja 04 ver. 2</h1>
+      <h3>wprowadzamy stan aplikacji jako obiekt zamiast textu</h3>
       <ul>
         <li>Prezentacja obiektu stanu myAllData ( state )</li>
         <li>obrazek stanu</li>
         myAllData &#123; text: 'Starwars', number: 9, array: [Ray, Luke, Leila],
         &#125;
+        <li>obrazek stanu</li>
       </ul>
     </div>
   );
